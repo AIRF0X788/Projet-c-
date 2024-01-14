4852917f46b469f282d369c2577ad57e3243f503
Guillaume L <gui2legrand2@gmail.com>
2024-01-14 19:21:08 +0100

    s

--- obj/Debug/net7.0/ProjetCSGHK.AssemblyInfo.cs
+++ obj/Debug/net7.0/ProjetCSGHK.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetCSGHK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2893d2b5ef441e14d6baa93d8dfc840a89282a08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetCSGHK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetCSGHK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
